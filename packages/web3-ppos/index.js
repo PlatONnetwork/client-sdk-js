a05cc7825109b1c322a6f332c19d7ae65e1fc078
szfdiwang <szfdiwang@163.com>
2022-09-06 16:02:27 +0800

    Processing of new APIs, including (1005 / 1006 / 5000)

--- packages/web3-ppos/index.js
+++ packages/web3-ppos/index.js
@@ -1,68 +1,120 @@
 var RLP = require('rlp');
 var EU = require('ethereumjs-util');
 var BN = require('bn.js');
+var BigInteger = require("big-integer");
 var Common = require('ethereumjs-common');
 var EthereumTx = require('ethereumjs-tx');
 var axios = require('axios');
-var utils = require('web3-utils');
+var utils = require('../web3-utils');
 const paramsOrder = {
-    '1000': ['typ', 'benefitAddress', 'nodeId', 'externalId', 'nodeName', 'website', 'details', 'amount', 'rewardPer', 'programVersion', 'programVersionSign', 'blsPubKey', 'blsProof'],
-    '1001': ['benefitAddress', 'nodeId', 'rewardPer', 'externalId', 'nodeName', 'website', 'details'],
-    '1002': ['nodeId', 'typ', 'amount'],
-    '1003': ['nodeId'],
-    '1004': ['typ', 'nodeId', 'amount'],
-    '1005': ['stakingBlockNum', 'nodeId', 'amount'],
+    '1000': [ 'typ', 'benefitAddress', 'nodeId', 'externalId', 'nodeName', 'website', 'details', 'amount', 'rewardPer', 'programVersion', 'programVersionSign', 'blsPubKey', 'blsProof' ],
+    '1001': [ 'benefitAddress', 'nodeId', 'rewardPer', 'externalId', 'nodeName', 'website', 'details' ],
+    '1002': [ 'nodeId', 'typ', 'amount' ],
+    '1003': [ 'nodeId' ],
+    '1004': [ 'typ', 'nodeId', 'amount' ],
+    '1005': [ 'stakingBlockNum', 'nodeId', 'amount' ],
+    '1006': [],
     '1100': [],
     '1101': [],
     '1102': [],
-    '1103': ['addr'],
-    '1104': ['stakingBlockNum', 'delAddr', 'nodeId'],
-    '1105': ['nodeId'],
+    '1103': [ 'addr' ],
+    '1104': [ 'stakingBlockNum', 'delAddr', 'nodeId' ],
+    '1105': [ 'nodeId' ],
+    '1106': [ 'delAddr' ],
     '1200': [],
     '1201': [],
     '1202': [],
 
-    '2000': ['verifier', 'pIDID'],
-    '2001': ['verifier', 'pIDID', 'newVersion', 'endVotingRounds'],
-    '2002': ['verifier', 'pIDID', 'module', 'name', 'newValue'],
-    '2005': ['verifier', 'pIDID', 'endVotingRounds', 'tobeCanceledProposalID'],
-    '2003': ['verifier', 'proposalID', 'option', 'programVersion', 'versionSign'],
-    '2004': ['verifier', 'programVersion', 'versionSign'],
-    '2100': ['proposalID'],
-    '2101': ['proposalID'],
+    '2000': [ 'verifier', 'pIDID' ],
+    '2001': [ 'verifier', 'pIDID', 'newVersion', 'endVotingRounds' ],
+    '2002': [ 'verifier', 'pIDID', 'module', 'name', 'newValue' ],
+    '2005': [ 'verifier', 'pIDID', 'endVotingRounds', 'tobeCanceledProposalID' ],
+    '2003': [ 'verifier', 'proposalID', 'option', 'programVersion', 'versionSign' ],
+    '2004': [ 'verifier', 'programVersion', 'versionSign' ],
+    '2100': [ 'proposalID' ],
+    '2101': [ 'proposalID' ],
     '2102': [],
     '2103': [],
-    '2104': ['module', 'name'],
-    '2105': ['proposalID', 'blockHash'],
-    '2106': ['module'],
+    '2104': [ 'module', 'name' ],
+    '2105': [ 'proposalID', 'blockHash' ],
+    '2106': [ 'module' ],
 
-    '3000': ['typ', 'data'],
-    '3001': ['typ', 'addr', 'blockNumber'],
+    '3000': [ 'typ', 'data' ],
+    '3001': [ 'typ', 'addr', 'blockNumber' ],
 
-    '4000': ['account', 'plan'],
-    '4100': ['account'],
+    '4000': [ 'account', 'plan' ],
+    '4100': [ 'account' ],
 
     '5000': [],
-    '5100': ['address', 'nodeIDs'],
+    '5100': [ 'address', 'nodeIDs' ],
 }
 
-function decodeBlockLogs(block) {
+
+const responseOrder = {
+    '1005': [ 'code', 'delegateIncome', 'released', 'restrictingPlan', 'lockReleased', 'lockRestrictingPlan' ],
+    '1006': [ 'code', 'released', 'restrictingPlan' ],
+    '5000': [ 'code', 'incomeArr' ],
+}
+
+
+const _bufferToString = (buffer) => {
+    return RLP.decode(buffer).toString()
+}
+
+const _bufferToBigInt = (buffer) => {
+    return BigInteger(RLP.decode(buffer).toString('hex'), 16)
+}
+
+function decodeBlockLogs(block, type = '') {
     let logs = block.logs;
     if (Array.isArray(logs)) {
-        for (let log of logs) {
-            try {
-                log.dataStr = JSON.parse(RLP.decode(log.data).toString());
-            } catch (error) { }
+        try {
+            let msg = {}
+            const codeList = RLP.decode(logs[ 0 ].data)
+            if (Array.isArray(codeList) && responseOrder[ type ]) {
+                const arr = responseOrder[ type ]
+                for (let [ index, code ] of codeList.entries()) {
+                    if (index === 0) {
+                        msg[ arr[ index ] ] = _bufferToString(code)
+                    } else {
+                        if (type === '5000') {
+                            msg[ arr[ index ] ] = RLP.decode(code)
+                        } else {
+                            msg[ arr[ index ] ] = _bufferToBigInt(code)//BigInteger(RLP.decode(code).toString('hex'), 16)
+                        }
+                    }
+                }
+            }
+            if (type === '5000' && msg.code === '0') {
+                // 5000
+                let tempObj = {}
+                const specType = [
+                    { 'key': 'NodeID' },
+                    { 'key': 'StakingNum' },
+                    { 'key': 'Reward' },
+                ]
+                for (let [ index, item ] of msg[ 'incomeArr' ][ 0 ].entries()) {
+                    if (index === 0) {
+                        tempObj[ specType[ index ].key ] = Buffer.from(item).toString('hex')
+                    } else {
+                        tempObj[ specType[ index ].key ] = BigInteger(Buffer.from(item).toString('hex'), 16)// BigInteger(RLP.decode(item).toString('hex'), 16)
+                    }
+                }
+                msg[ 'incomeArr' ][ 0 ] = tempObj
+            }
+            logs[ 0 ].dataStr = JSON.stringify(msg)
+        } catch (error) {
+            console.log(error);
         }
     }
 }
 
 function objToParams(params) {
     if (!Array.isArray(params)) {
-        let pars = [params.funcType]
-        let order = paramsOrder[params.funcType];
+        let pars = [ params.funcType ]
+        let order = paramsOrder[ params.funcType ];
         for (const key of order) {
-            pars.push(params[key])
+            pars.push(params[ key ])
         }
         return pars;
     }
@@ -200,7 +252,7 @@ PPOS.prototype.rpc = async function (method, params) {
         const data = { "jsonrpc": "2.0", "method": method, "params": params, "id": new Date().getTime() }
         let replay = await this.client.post("", data);
         if (replay.status === 200) {
-            if(undefined === replay.data.result && undefined != replay.data.error) {
+            if (undefined === replay.data.result && undefined != replay.data.error) {
                 return Promise.reject(replay.data.error);
             } else {
                 return Promise.resolve(replay.data.result);
@@ -218,8 +270,8 @@ PPOS.prototype.call = async function (params) {
         let rawTx = {};
         params = objToParams(params);
         rawTx.data = paramsToData(params);
-        rawTx.to = funcTypeToBech32(this.hrp, params[0]);
-        let data = await this.rpc("platon_call", [rawTx, "latest"]);
+        rawTx.to = funcTypeToBech32(this.hrp, params[ 0 ]);
+        let data = await this.rpc("platon_call", [ rawTx, "latest" ]);
         return Promise.resolve(pposHexToObj(data));
     } catch (error) {
         return Promise.reject(error);
@@ -231,47 +283,46 @@ PPOS.prototype.send = async function (params, other) {
     try {
         let privateKey = this.privateKey;
         let chainId = this.chainId;
-        
+        let funcType;
         let address = EU.bufferToHex(EU.privateToAddress('0x' + privateKey));
         var bech32Address = utils.toBech32Address(this.hrp, address)
-        let nonce = await this.rpc("platon_getTransactionCount", [bech32Address, 'latest']);
+        let nonce = await this.rpc("platon_getTransactionCount", [ bech32Address, 'latest' ]);
         let rawTx = {};
+        funcType = params[ 'funcType' ]
         params = objToParams(params);
         rawTx.data = paramsToData(params);
-        
+
         rawTx.from = address;
-        rawTx.to = funcTypeToAddress(params[0]);
+        rawTx.to = funcTypeToAddress(params[ 0 ]);
         rawTx.gasPrice = (other && other.gasPrice) || this.gasPrice || '0x746a528800';
         rawTx.nonce = nonce;
-        if("0x1000000000000000000000000000000000000005" == rawTx.to)
-        {
+        if ("0x1000000000000000000000000000000000000005" == rawTx.to) {
             var es_tx = {
-                from:utils.toBech32Address(this.hrp, rawTx.from),
+                from: utils.toBech32Address(this.hrp, rawTx.from),
                 to: utils.toBech32Address(this.hrp, rawTx.to),
                 data: rawTx.data,
                 gasPrice: rawTx.gasPrice,
                 nonce: rawTx.nonce
             };
             // Estimate the governance interface
-            rawTx.gas = await this.rpc("platon_estimateGas", [es_tx])
+            rawTx.gas = await this.rpc("platon_estimateGas", [ es_tx ])
             console.log("estimateGas:", rawTx.gas);
         }
-        else
-        {
+        else {
             rawTx.gas = (other && other.gas) || this.gas || '0xf4240';
-        }        
+        }
 
         let rawTransaction = signTx(privateKey, chainId, rawTx);
-        let hash = await this.rpc("platon_sendRawTransaction", [rawTransaction]);
+        let hash = await this.rpc("platon_sendRawTransaction", [ rawTransaction ]);
         if (!hash) return Promise.reject('no hash');
 
         let retry = (other && other.retry) || this.retry || 600;
         let interval = (other && other.interval) || this.interval || 100;
         const errMsg = `getTransactionReceipt txHash ${hash} interval ${interval}ms by ${retry} retry failed`;
         while (retry) {
-            const receipt = await this.rpc('platon_getTransactionReceipt', [hash]);
+            const receipt = await this.rpc('platon_getTransactionReceipt', [ hash ]);
             if (receipt) {
-                decodeBlockLogs(receipt);
+                decodeBlockLogs(receipt, funcType.toString());
                 return Promise.resolve(receipt);
             }
             await sleep(interval);
@@ -286,3 +337,5 @@ PPOS.prototype.send = async function (params, other) {
 PPOS.prototype.signTx = signTx;
 
 module.exports = PPOS;
+
+
